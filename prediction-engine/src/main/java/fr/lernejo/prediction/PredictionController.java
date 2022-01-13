2b087a247899831bf4d628e254116f90c3a02782
zahra-khf <zahrakhalifa190@gmail.com>
2022-01-14 00:30:37 +0100

    part 5 fix

--- prediction-engine/src/main/java/fr/lernejo/prediction/PredictionController.java
+++ prediction-engine/src/main/java/fr/lernejo/prediction/PredictionController.java
@@ -16,24 +16,25 @@ import static java.util.Map.copyOf;
 @RestController
 public class PredictionController {
     private final TemperatureService temperatureService = new TemperatureService();
+    private final LocalDate date = LocalDate.now();
 
-    @GetMapping(path = "/api/temperatrue")
+    private void addToTemperatures(String date, double temps, ArrayList temperatures){
+        HashMap<String, Object> temp = new HashMap<String, Object>();
+        temp.put("date", date);
+        temp.put("temperature", Math.round(temps));
+        temperatures.add(temp.clone());
+    }
+
+    @GetMapping(path = "/api/temperature")
     Object getTemprature(@RequestParam String country) {
         try {
-            double tempratureResult1 = temperatureService.getTemperature(country);
-            double tempratureResult2 = temperatureService.getTemperature(country);
             ArrayList tempratureList = new ArrayList();
-            LocalDate date = LocalDate.now();
             HashMap<String, Object> response = new HashMap<String, Object>();
             HashMap<String, Object> temp = new HashMap<String, Object>();
-            temp.put("date", date.minusDays(1).toString());
-            temp.put("temprature", tempratureResult1);
-            tempratureList.add(temp.clone());
-            temp.put("date", date.minusDays(2).toString());
-            temp.put("temprature", tempratureResult2);
-            tempratureList.add(temp.clone());
+            addToTemperatures(date.minusDays(1).toString(),temperatureService.getTemperature(country),tempratureList);
+            addToTemperatures(date.minusDays(2).toString(),temperatureService.getTemperature(country),tempratureList);
             response.put("country", country);
-            response.put("tempratures", tempratureList);
+            response.put("temperatures", tempratureList);
             return response;
         }catch (UnknownCountryException e){
             return ResponseEntity.status(417).body(e.getMessage());
